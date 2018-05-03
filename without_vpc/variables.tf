0aee01c06b18c1254b07954bfb96ae5efb126081
Avi Cavale <avi@shippable.com>
2018-05-03 09:17:51 -0700

    adding without vpc

---
+++ without_vpc/variables.tf
@@ -0,0 +1,62 @@
+# main creds for AWS connection
+variable "accessKey" {
+  description = "AWS access key"
+}
+
+variable "secretKey" {
+  description = "AWS secert access key"
+}
+
+variable "region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "avl_zone" {
+  description = "availability zone used for the beta"
+  default = "us-east-1b"
+}
+
+# this is a keyName for key pairs
+variable "aws_key_name" {
+  description = "Key Pair Name used to login to the box"
+}
+
+# all variables related to VPC
+variable "install_version" {
+  description = "version of the infra"
+}
+
+variable "vpc_id" {
+  description = "VPC ID where everything will be created"
+}
+
+
+variable "ig_id" {
+  description = "Internet Gateway ID opf the VPC"
+}
+
+
+variable "ship_vpc_cidr" {
+  description = "Uber IP addressing for the Network"
+}
+
+variable "ship_sn_cidr" {
+  description = "Public 0.0 CIDR for externally accesible subnet"
+}
+
+variable "in_type_cp" {
+  description = "AWS Instance type for control plane server"
+}
+
+variable "in_type_bp" {
+  description = "AWS Instance type for running builds"
+}
+
+variable "ami_us_east_1_ubuntu1604"{
+  description = "AWS AMI for us-east-1 Ubuntu 16.04"
+}
+
+variable "acm_cert_arn"{
+  description = "acm cert arn"
+}
---
+++ without_vpc/vpc.tf
@@ -0,0 +1,98 @@
+#========================== 0.0 Subnet =============================
+
+# Public subnet
+resource "aws_subnet" "ship_sn" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "${var.ship_sn_cidr}"
+  availability_zone = "${var.avl_zone}"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "ship_sn_${var.install_version}"
+  }
+}
+
+# Routing table for public subnet
+resource "aws_route_table" "ship_rt_public" {
+  vpc_id = "${var.vpc_id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${var.ig_id}"
+  }
+  tags {
+    Name = "ship_rt_public_${var.install_version}"
+  }
+}
+
+# Associate the routing table to public subnet
+resource "aws_route_table_association" "rt_assn_public" {
+  subnet_id = "${aws_subnet.ship_sn.id}"
+  route_table_id = "${aws_route_table.ship_rt_public.id}"
+}
+
+#========================== SG =============================
+
+resource "aws_security_group" "ship_sg" {
+  name = "ship_sg_${var.install_version}"
+  description = "Shippable external traffic security group"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 5671
+    to_port = 5671
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 15671
+    to_port = 15671
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = "3389"
+    to_port   = "3389"
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+    # allow all traffic to private SN
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = [
+      "${var.ship_sn_cidr}",
+      "0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "ship_sg_${var.install_version}"
+  }
+}
